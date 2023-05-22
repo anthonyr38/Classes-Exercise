3fd4f6674c2bac32089398a4f1f9215ee68bdefe
Anthony Rodriguez <anthonyr38@proton.me>
2023-05-22 12:50:25 -0400

    Classes Exercise 1

---
+++ Classes/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    
+    
+    public class Car
+    {
+        public Car()
+        {
+            
+        }
+
+        public Car(string makeInp, string modelInp, int yearInp)
+        {
+            Make = makeInp;
+            Model = modelInp;
+            Year = yearInp;
+        }
+                                   
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car carA = new Car();
+            carA.Make = "Mitsubishi";
+            carA.Model = "Outlander";
+            carA.Year = 2022;
+
+            var carB = new Car()
+            {
+                Make = "Chevrolet",
+                Model = "Cruze",
+                Year = 2019
+            };
+        
+            
+            var carC = new Car("Nissan", "Versa", 2018);
+
+            var listOfCars = new List<Car>();
+
+            listOfCars.Add(carA);
+            listOfCars.Add(carB);
+            listOfCars.Add(carC);
+
+            foreach(var auto in listOfCars) 
+            {
+                Console.WriteLine($"{auto.Make} {auto.Model} {auto.Year}");
+            }
         }
+
+
+
     }
 }
